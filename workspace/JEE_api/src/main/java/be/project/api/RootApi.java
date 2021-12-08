d9d8f93739b7b11ba318a3cca753d1afd0687243
simondifalco13 <61333124+simondifalco13@users.noreply.github.com>
2021-12-08 18:24:36 +0100

    Creation of API project in workspace

---
+++ workspace/JEE_api/src/main/java/be/project/api/RootApi.java
@@ -0,0 +1,13 @@
+package be.project.api;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RootApi extends Application{
+
+	public RootApi() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
