3cbc02081ec009196acc1575df7916b72dbe25a5
simondifalco13 <61333124+simondifalco13@users.noreply.github.com>
2021-12-21 19:35:16 +0100

    leaderDAO in API

--- workspace/JEE_Mehdi_Simon/src/main/java/be/project/dao/LeaderDAO.java
+++ workspace/JEE_Mehdi_Simon/src/main/java/be/project/dao/LeaderDAO.java
@@ -1,11 +1,52 @@
 package be.project.dao;
 
+import java.net.URI;
 import java.util.ArrayList;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+import be.project.javabeans.Employee;
 import be.project.javabeans.Leader;
 
 public class LeaderDAO implements DAO<Leader> {
 
+	private static  String apiUrl;
+	private Client client;
+	private WebResource resource;
+	
+	private static URI getBaseUri() {
+		return UriBuilder.fromUri(apiUrl).build();
+	}
+	
+	private static String getApiUrl() {
+		Context ctx;
+		String api="";
+		try {
+			ctx = new InitialContext();
+			Context env = (Context) ctx.lookup("java:comp/env");
+		    api= (String) env.lookup("apiUrl");
+		} catch (NamingException e) {
+			System.out.println("Error to get api url");
+		}
+		return api;
+	}
+	
+	public LeaderDAO() {
+		ClientConfig config=new DefaultClientConfig();
+		client=Client.create(config);
+		apiUrl=getApiUrl();
+		resource=client.resource(getBaseUri());
+	}
 	@Override
 	public boolean insert(Leader obj) {
 		// TODO Auto-generated method stub
@@ -26,8 +67,21 @@ public class LeaderDAO implements DAO<Leader> {
 
 	@Override
 	public Leader find(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		String responseJSON=resource
+				.path("leader")
+				.path(String.valueOf(id))
+				.accept(MediaType.APPLICATION_JSON)
+				.get(String.class);
+		Leader leader=null;
+		ObjectMapper mapper=new ObjectMapper();
+		try {
+			leader=(Leader) mapper.readValue(responseJSON, Leader.class);
+			System.out.println("leader : "+leader.getFirstname());
+			return leader;
+		} catch (Exception e) {
+			System.out.println("error = "+e.getMessage());
+			return null;
+		}
 	}
 
 	@Override
--- workspace/JEE_api/src/main/java/be/project/api/LeaderAPI.java
+++ workspace/JEE_api/src/main/java/be/project/api/LeaderAPI.java
@@ -1,6 +1,15 @@
 package be.project.api;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import be.project.models.Employee;
+import be.project.models.Leader;
 
 @Path("/leader")
 public class LeaderAPI {
@@ -9,4 +18,12 @@ public class LeaderAPI {
 		// TODO Auto-generated constructor stub
 	}
 
+	//verifier qu'on recoit bien la clé de l'api
+		@GET
+		@Path("{serialNumber}")
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response getLeader(@PathParam("serialNumber") int serialNumber) {
+			Leader leader=Leader.getLeader(serialNumber);
+			return Response.status(Status.OK).entity(leader).build();
+		}
 }
